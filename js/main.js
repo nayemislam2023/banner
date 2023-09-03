e1e86e51d9dc5aa4496735ac1e9c53152efe8e3e
Nayem Islam <143949943+nayemislam2023@users.noreply.github.com>
2023-09-03 22:26:16 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,76 @@
+
+$(function(){
+	/*$('.slider').slick({
+		infinite:true,
+		slidesToShow:3,
+		slidesToScroll:1,
+		arrows:false,
+		autoplay:true,
+		autoplaySpeed:500,
+		
+	});*/
+	
+	
+	$('.slider-slick').slick({
+		infinite:true,
+		slidesToShow:1,
+		slidesToScroll:1,
+		arrows:true,
+		autoplay:true,
+		autoplaySpeed:800,
+		fade :true,
+		prevArrow : '<i class="fa fa-arrow-left"></i>',
+		nextArrow : '<i class="fa fa-arrow-right"></i>',
+		dots : false,
+		
+	});
+	
+	
+	
+	
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+$(function () {
+    $('.slider').slick({
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        arrows : false,
+        autoplay : true,
+        autoplaySpeed : 500,
+    });
+    
+    $('.slider-slick').slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows : true,
+        autoplay : true,
+        fade : true,
+        prevArrow : ' <i class="fa fa-arrow-left"></i>',
+        nextArrow : ' <i class="fa fa-arrow-right"></i>',
+        dots : true
+    });
+})
+*/
